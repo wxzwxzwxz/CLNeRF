f32d3fffaff14be0828500f5e621d279fdafbccf
urnotpopcorn <lumosomul@gmail.com>
2023-04-07 16:24:25 +0800

    commit

--- print_value_iteration.py
+++ print_value_iteration.py
@@ -13,12 +13,14 @@ render_list = sorted(render_list)
 print(algo)
 
 for render_dir in render_list:
-    if 'renderonly_test' not in render_dir or 'v7v8' not in render_dir:
+    # if 'renderonly_test' not in render_dir or 'v7v8' not in render_dir:
+    if 'renderonly_test' not in render_dir or 'v7v9' not in render_dir:
         continue
 
     psnr_path = os.path.join(input_dir, render_dir, 'psnr.txt')
     if not os.path.exists(psnr_path):
         continue 
+    # print(psnr_path)
     # print(render_dir.split('renderonly_test_')[-1], end='\t')
     with open(psnr_path, 'r') as f:
         for line in f:
@@ -29,12 +31,14 @@ print()
 print()
 
 for render_dir in render_list:
-    if 'renderonly_test' not in render_dir or 'v7v8' in render_dir:
+    # if 'renderonly_test' not in render_dir or 'v7v8' in render_dir:
+    if 'renderonly_test' not in render_dir or 'v7v9' in render_dir:
         continue
 
     psnr_path = os.path.join(input_dir, render_dir, 'psnr.txt')
     if not os.path.exists(psnr_path):
         continue 
+    
     # print(render_dir.split('renderonly_test_')[-1], end='\t')
     with open(psnr_path, 'r') as f:
         for line in f:
