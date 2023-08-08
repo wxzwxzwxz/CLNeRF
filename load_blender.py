deb05028fb34415c825e3f810213e240c66e19cc
urnotpopcorn <lumosomul@gmail.com>
2023-08-08 16:25:54 +0800

    add py

--- load_blender.py
+++ load_blender.py
@@ -262,8 +262,6 @@ def load_blender_data(args, basedir, half_res=False, testskip=1,
         poses = np.concatenate(all_poses, 0)
         all_paths = np.array(all_paths)
         
-        print(ori_H, meta['h'])
-        input()
         # H, W = int(meta['h']), int(meta['w'])#ori_H, ori_W change 0523wp
         H, W = ori_H, ori_W # int(meta['img_h']), int(meta['img_w'])
         
